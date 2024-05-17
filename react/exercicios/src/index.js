8ffafb43d544baaedbee7e54e14f7f7a6b7cf237
Angélica Martiniano <angiihsilva@gmail.com>
2024-05-17 11:11:00 -0300

    `parametros e componentes multiplos`

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 
 import Pai from './components/Pai'
 import Filho from './components/Filho'
+import Saudacao from './components/Saudacao'
+import { BoaTarde, BoaNoite} from './components/Multiplos'
+import BomDia from './components/BomDia'
 
 // ReactDOM.render(<h1>React 2</h1>, document.getElementById('root')) //depreciado
 
@@ -11,10 +14,17 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <div>
-    <Pai nome="Paulo" sobrenome="Silva">
+    {/* <Pai nome="Paulo" sobrenome="Silva">
       <Filho nome="Pedro" />
       <Filho nome="Paulo" />
       <Filho nome="Carla" />
-    </Pai>
+    </Pai> */}
+
+
+    {/* <BomDia nome="Gustavo"/> */}
+
+
+    <BoaTarde nome="Marildes"/>
+    <BoaNoite nome="Marina"/>
   </div>
 );
