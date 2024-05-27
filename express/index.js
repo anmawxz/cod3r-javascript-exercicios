3a03a9798a3f4f050b7975c0a6cfcf1b0c85fa84
Angélica Martiniano <angiihsilva@gmail.com>
2024-05-27 11:08:53 -0300

    `tratando requisição`

--- express/index.js
+++ express/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
 const app = express()
+
+app.use((req, res) => {
+    res.send('Estou <b>bem!</b>')
+})
 
 app.listen(3000, ()=> {
     console.log("backend funcionando...")
