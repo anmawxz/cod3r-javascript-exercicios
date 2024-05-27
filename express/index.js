7fba426d12be3c515fcb7e9d2daa10a6fcfbb668
Angélica Martiniano <angiihsilva@gmail.com>
2024-05-27 12:11:03 -0300

    `exemplo bodyparser`

--- express/index.js
+++ express/index.js
@@ -1,40 +1,70 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use('/opa', (req, res, next) => {
-    console.log('Antes...')
-    next();
-})
+const saudacao = require("./saudacaoMid");
+const bodyParser = require("body-parser");
 
-app.get('/opa', (req, res, next) => {
-    console.log('Durante...')
-    res.json({
-        data: [
-            {id: 7, name: 'Ana', position: 1 },
-            {id: 34, name: 'Bia', position: 2 },
-            {id: 73, name: 'Carlos', position: 3 }
-        ],
-        count: 30,
-        skip: 0,
-        limit: 3,
-        status: 200
-    })
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(saudacao("Alexandre"));
 
-    next()
-    
-    // res.json({
-    //     name: 'iPad 32Gb',
-    //     price: 1899.00,
-    //     discount: 0.12
-    // })
+app.use((req, res, next) => {
+  console.log("Antes...");
+  next();
+});
 
-    // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
-})
+app.get("/clientes/relatorio", (req, res) => {
+  res.send(
+    `Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`
+  );
+});
 
-app.use('/opa', (req, res) => {
-    console.log('Depoois...')
-})
+app.post("/corpo", (req, res) => {
+  //   let corpo = "";
+  //   req.on("data", function (parte) {
+  //     corpo += parte;
+  //   });
+  //   req.on("end", function () {
+  //     res.send(corpo);
+  //   });
 
-app.listen(3000, ()=> {
-    console.log("backend funcionando...")
-})
+  res.send(req.body);
+});
+
+app.get("/clientes/:id", (req, res) => {
+  res.send(`Cliente ${req.params.id} selecionado!`);
+});
+
+app.get("/opa", (req, res, next) => {
+  console.log("Durante...");
+  res.json({
+    data: [
+      { id: 7, name: "Ana", position: 1 },
+      { id: 34, name: "Bia", position: 2 },
+      { id: 73, name: "Carlos", position: 3 },
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200,
+  });
+
+  next();
+
+  // res.json({
+  //     name: 'iPad 32Gb',
+  //     price: 1899.00,
+  //     discount: 0.12
+  // })
+
+  // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+});
+
+app.use("/opa", (req, res) => {
+  console.log("Depoois...");
+});
+
+app.listen(3000, () => {
+  console.log("backend funcionando...");
+});
